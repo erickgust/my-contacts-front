f6cc765ea0d91798b5e8ce558ffca9897ed65786
Erick Gustavo <erick.stazs@gmail.com>
2022-10-03 15:57:08 -0300

    Clear app

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,6 @@
-import { useState } from 'react'
-
 function App () {
-  const [counter, setCounter] = useState(0)
-
-  function handleIncrement () {
-    setCounter(counter => counter + 1)
-  }
-
-  function handleDecrement () {
-    setCounter(counter => counter - 1)
-  }
-
   return (
-    <>
-      <h1>{counter}</h1>
-      <button onClick={handleDecrement}>-</button>
-      <button onClick={handleIncrement}>+</button>
-    </>
+    <h1>My Contacts</h1>
   )
 }
 
