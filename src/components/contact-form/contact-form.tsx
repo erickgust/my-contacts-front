3def5958cae2d5b824cf22bc5375426bdec9e6a2
Erick Gustavo <erick.stazs@gmail.com>
2023-01-03 22:43:35 -0300

    Add controlled components

--- src/components/contact-form/contact-form.tsx
+++ src/components/contact-form/contact-form.tsx
@@ -2,6 +2,7 @@ import { FormGroup } from '@/components/form-group'
 import { Button } from '@/ui/button'
 import { Input } from '@/ui/input'
 import { Select } from '@/ui/select'
+import { useState } from 'react'
 import * as S from './contact-form-styles'
 
 type ContactFormProps = {
@@ -9,25 +10,52 @@ type ContactFormProps = {
 }
 
 export function ContactForm ({ buttonLabel }: ContactFormProps) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [category, setCategory] = useState('')
+
+  function handleSubmit (e: React.FormEvent) {
+    e.preventDefault()
+    console.log({ name, email, phone, category })
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder='Nome' />
-      </FormGroup>
-
-      <FormGroup
-        error='O formato do e-mail é inválido.'
-      >
-        <Input placeholder='E-mail' error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder='Telefone' />
+        <Input
+          placeholder='Nome'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          placeholder='E-mail'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder='Telefone'
+          value={phone}
+          onChange={e => setPhone(e.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={e => setCategory(e.target.value)}
+        >
+          <option value="">Selecione uma categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="twitter">Twitter</option>
+          <option value="facebook">Facebook</option>
+          <option value="linkedin">Linkedin</option>
         </Select>
       </FormGroup>
 
