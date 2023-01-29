0b00733151049584dd2d6db8325e26903e53ace9
Erick Gustavo <erick.stazs@gmail.com>
2023-01-29 15:04:41 -0300

    Ref: create request and createRequest abstractions

--- src/services/utils/http.ts
+++ src/services/utils/http.ts
@@ -8,10 +8,10 @@ class HttpClient {
     this.baseUrl = baseUrl
   }
 
-  async get (path: string) {
+  async request (path: string, options?: RequestInit) {
     await delay(500)
 
-    const response = await fetch(this.baseUrl + path)
+    const response = await fetch(this.baseUrl + path, options)
     const contentType = response.headers.get('Content-Type')
 
     if (!contentType?.includes('application/json')) {
@@ -25,6 +25,32 @@ class HttpClient {
     }
 
     throw new APIError(response, body)
+  }
+
+  createRequest <T> (method: string) {
+    return (path: string, body: T) => {
+      return this.request(path, {
+        method,
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    }
+  }
+
+  get (path: string) {
+    return this.request(path)
+  }
+
+  post <T> (path: string, body: T) {
+    const request = this.createRequest('POST')
+    return request(path, body)
+  }
+
+  put <T> (path: string, body: T) {
+    const request = this.createRequest('PUT')
+    return request(path, body)
   }
 }
 
