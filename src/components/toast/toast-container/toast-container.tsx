e4be96f29fc19bb4b486c0e7ec626cf23b0ab015
Erick Gustavo <erick.stazs@gmail.com>
2023-07-21 16:43:24 -0300

    ref(toast): move useToastContainer logic to ToastContainer

--- src/components/toast/toast-container/toast-container.tsx
+++ src/components/toast/toast-container/toast-container.tsx
@@ -1,12 +1,33 @@
+import { useEffect } from 'react'
 import { ToastMessage } from '../toast-message'
+
 import * as S from './container-styles'
-import { useToastContainer } from './use-toast-container'
+import { Toast, ToastData } from '../toast-types'
+
+import { useAnimatedList } from '@/resources/use-animated-list'
+import { toastEventManager } from '@/utils/toast'
 
 export function ToastContainer () {
   const {
     handleRemoveItem,
+    setItems: setToasts,
     renderList,
-  } = useToastContainer()
+  } = useAnimatedList<Toast>()
+
+  useEffect(() => {
+    const handleAddToast = ({ message, type, duration }: ToastData) => {
+      setToasts((prevToasts) => [
+        ...prevToasts,
+        { message, type, duration, id: Math.random() },
+      ])
+    }
+
+    toastEventManager.on('addtoast', handleAddToast)
+
+    return () => {
+      toastEventManager.off('addtoast', handleAddToast)
+    }
+  }, [setToasts])
 
   return (
     <S.Container>
