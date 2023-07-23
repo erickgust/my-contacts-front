4d6bb609f3d3a41172ce276e8d95e1b7f17ec50a
Erick Gustavo <erick.stazs@gmail.com>
2023-07-22 22:57:07 -0300

    fix: wrap children in fragment

--- src/components/portal/portal.tsx
+++ src/components/portal/portal.tsx
@@ -8,14 +8,15 @@ type PortalProps = {
 
 export function Portal ({ containerName = 'root', children }: PortalProps) {
   const containerAlreadyExists = document.querySelector(`[data-portal="${containerName}"]`)
+  const childrenNode = <>{children}</>
 
   if (containerAlreadyExists) {
-    return createPortal(children, containerAlreadyExists)
+    return createPortal(childrenNode, containerAlreadyExists)
   }
 
   const container = document.createElement('div')
   container.setAttribute('data-portal', containerName)
   document.body.appendChild(container)
 
-  return createPortal(children, container)
+  return createPortal(childrenNode, container)
 }
